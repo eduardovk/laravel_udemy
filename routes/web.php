bb7c7d504c0196e7acbfce2eaf304ca3d955c1b2
eduardovk <eduardo_vk@hotmail.com>
2019-08-12 23:27:47 -0300

    Teste de rotas inicial

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// {variavel?} ponto de interrogacao para tornar o parametro opcional
+Route::get('/contato/{id?}', function($id = null){
+  return "Contato! ID: $id";
+});
+
+Route::post('/contato', function($id = null){
+  //dd($_POST);  util para debug de variavies post
+  return "Contato! ID: $id";
+});
+
+Route::put('/contato', function($id = null){
+  dd($_POST);  
+  return "Contato! ID: $id";
+});
