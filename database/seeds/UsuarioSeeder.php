38e5c64b99c586f2e061c6d5f5d175d4274d5608
eduardovk <eduardo_vk@hotmail.com>
2019-08-20 21:50:03 -0300

    Criada seeder de Usuario

---
+++ database/seeds/UsuarioSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dados = [
+                'name'=>'Eduardo',
+                'email'=>'admin@mail.com',
+                'password'=>bcrypt('123456'),
+        ];
+        //checa se ja existe usuario com este email
+        if(User::where('email','=',$dados['email'])->count()){ //se ja existe, atualiza
+            $usuario = User::where('email', '=', $dados['email'])->first();
+            $usuario->update($dados);
+        }else{ //se nao existe, cria
+            User::create($dados);
+        }
+    }
+}
